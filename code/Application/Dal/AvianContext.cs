466734ed812abbfddf6af81ba3c7cf4353b29ddb
Johnny-18 <60463530+Johnny-18@users.noreply.github.com>
2023-10-31 23:03:00 +0200

    add empty project

---
+++ code/Application/Dal/AvianContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Dal
+{
+    class AvianContext
+    {
+    }
+}
---
+++ code/Avian/Login.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Avian
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextEmailChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextPasswordChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ButtonLogIn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
